fc58794cf82ffa78b84d51ab2473be0dede76d59
Whithmann Obando <whithmann_98@hotmail.com>
2022-10-11 22:40:49 -0500

    Fix: Nuevos cambios en ConfigSecurity

--- src/main/java/pe/idat/security/ConfigSecurity.java
+++ src/main/java/pe/idat/security/ConfigSecurity.java
@@ -22,20 +22,20 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class ConfigSecurity extends WebSecurityConfigurerAdapter{
 
 	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {		// en este caso, no es necesario usar el @BEAN
-		auth.inMemoryAuthentication().withUser("profesor").password(encriptado().encode("123")).roles("ADMIN");
-		auth.inMemoryAuthentication().withUser("estudiante").password(encriptado().encode("123")).roles("USER");
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {		// En este caso no es necesario usar el @BEAN
+		auth.inMemoryAuthentication().withUser("PROFESOR").password(encriptado().encode("123")).roles("ADMIN");
+		auth.inMemoryAuthentication().withUser("ALUMNO").password(encriptado().encode("123")).roles("USER");
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-		.antMatchers(HttpMethod.GET).access("hasRole('USER')")
-		.antMatchers("/producto/v1/*").access("hasRole('ADMIN')")
-		.and()
-		.httpBasic()
-		.and()
-		.csrf().disable();
+			.antMatchers("/producto/v1/guardar").access("hasRole('ADMIN')")
+			.antMatchers("/producto/v1/listar").permitAll()
+			.and()
+			.httpBasic()
+			.and()
+			.csrf().disable();
 	}
 
 	
